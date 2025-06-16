b143796a69053d0b5980cfd6878c8b5f7d411712
Brayan O <brayanramirez018@gmail.com>
2025-06-16 18:35:00 -0500

    ok2

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const estudiantes = [
+  {
+    nombre: "Ana Martínez",
+    calificaciones: [4.5, 3.8, 4.0, 4.7]
+  },
+  {
+    nombre: "Luis Gómez",
+    calificaciones: [3.2, 3.5, 4.1, 3.9]
+  },
+  {
+    nombre: "Camila Torres",
+    calificaciones: [4.8, 4.9, 5.0, 4.6]
+  },
+  {
+    nombre: "Julián Ramírez",
+    calificaciones: [2.9, 3.3, 3.7, 3.0]
+  }
+];
